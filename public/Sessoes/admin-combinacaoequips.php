2e7aaea7df888316201157c1caa109693c3056f7
Felipe Medeiros <medeiros.dev@gmail.com>
2021-03-21 14:06:26 -0300

    Listagem de Equips

--- public/Sessoes/admin-combinacaoequips.php
+++ public/Sessoes/admin-combinacaoequips.php
@@ -1,34 +1,44 @@
+<style type="text/css">
+    .equipamentos_casse_1 {
+        color: white;
+    }
+</style>
 <div class="panel-heading">
     Combinações
 </div>
 
 <div class="panel-body">
-    <?php $equipamentos = $connection->run("SELECT * FROM tb_item_equipamentos GROUP BY item ORDER BY lvl ASC")->fetch_all_array(); ?>
-    
-
-    <table class="table table-stripped">
+    <?php $equipamentos = $connection->run("SELECT * FROM tb_equipamentos ORDER BY categoria ASC, lvl ASC")->fetch_all_array(); ?>
+    <table class="table table-striped table-hover">
         <thead>
         <tr>
-            <th>ID item</th>
+            <th width="50" class="text-center">Id</th>
             <th colspan="2">Nome</th>
-            <th>level</th>
-            <th>Classes</th>
-            <th>Categoria do equipamento(cor)</th>
-            <th>Categoria do dano</th>
+            <th width="100" class="text-center">Nível</th>
+            <th width="150" class="text-center">Classe</th>
         </tr>
         </thead>
         <tbody>
         <?php foreach ( $equipamentos as $equipamento): ?>
-           <tr>
-            <td><?php echo $equipamento ['cod_equipamento'] ?></td>
-            <td><img src="Imagens/Itens/<?= $equipamento["img"] ?>.png"/></td>
-            <td><span class="equipamentos_casse_<?= $equipamento["categoria"] ?>">
-                <?php echo $equipamento ['nome'] ?></span></td>
-            <td><?php echo $equipamento ['lvl'] ?></td>
-            <td><?php echo $equipamento ['requisito'] ?></td>
-            <td><?php echo $equipamento ['categoria'] ?></td>
-            <td><?php echo $equipamento ['cat_dano'] ?></td>
-           </tr>
+            <tr>
+                <td style="vertical-align: middle;" class="text-center"><?php echo $equipamento['item'] ?></td>
+                <td><img src="Imagens/Itens/<?= $equipamento["img"] ?>.png"/></td>
+                <td style="vertical-align: middle;">
+                    <span class="equipamentos_casse_<?= $equipamento["categoria"] ?>">
+                        <?php echo $equipamento['nome'] ?>
+                    </span><br />
+                    <small><?php echo $equipamento['descricao'] ?></small>
+                </td>
+                <td style="vertical-align: middle;" class="text-center"><?php echo $equipamento ['lvl'] ?></td>
+                <td style="vertical-align: middle;" class="text-center">
+                    <?php
+                    if ($equipamento['requisito'] == '1')        echo 'Espadachim';
+                    elseif ($equipamento['requisito'] == '2')    echo 'Lutador';
+                    elseif ($equipamento['requisito'] == '3')    echo 'Atirador';
+                    else                                         echo 'Todas';
+                    ?>
+                </td>
+            </tr>
         <?php endforeach; ?>
         </tbody>
     </table>
