f37a8066dbbf255b1eccf236769197942844976b
Perguntation <80928871+Perguntation@users.noreply.github.com>
2021-04-27 16:41:40 -0300

    Update combate.php

--- public/Funcoes/combate.php
+++ public/Funcoes/combate.php
@@ -523,7 +523,7 @@ function calc_dano($pers, $alvo, $dano_hab = 0) {
     $esquiva    = chance_esquiva($pers, $alvo);
 
     $retorno["chance_esquiva"] = $esquiva;
-    $retorno["dado_esquivou"] = rand(1 * 10, 100 * 10) / 10;
+    $retorno["dado_esquivou"] = rand(1, 1000) / 10;
 
     if ($retorno["dado_esquivou"] <= $esquiva) {
         $retorno["esquivou"] = true;
@@ -534,7 +534,7 @@ function calc_dano($pers, $alvo, $dano_hab = 0) {
         $chance_crit    = chance_crit($pers, $alvo);
 
         $retorno["chance_critico"]  = $chance_crit;
-        $retorno["dado_critou"]     = rand(1 * 10, 100 * 10) / 10;
+        $retorno["dado_critou"]     = rand(1, 1000) / 10;
         if ($retorno["dado_critou"] <= $chance_crit) {
             $retorno["critou"] = true;
 
@@ -544,7 +544,7 @@ function calc_dano($pers, $alvo, $dano_hab = 0) {
         $chance_bloq = chance_bloq($pers, $alvo);
 
         $retorno["chance_bloqueio"] = $chance_bloq;
-        $retorno["dado_bloqueou"]   = rand(1 * 10, 100 * 10) / 10;
+        $retorno["dado_bloqueou"]   = rand(1, 1000) / 10;
         if ($retorno["dado_bloqueou"] <= $chance_bloq) {
             $retorno["bloqueou"] = true;
 
