210b39b7cb3ffadadcb13f4575af6d0d10a34e9a
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-04 17:29:06 -0300

    Update conectdb.php
    corrigido

--- public/Includes/conectdb.php
+++ public/Includes/conectdb.php
@@ -48,7 +48,7 @@ define('COD_CASCO_KAIROUSEKI',		2);
 
 define('SKILLS_ICONS_MAX',			512);
 
-define('PERSONAGENS_MAX',			500);
+define('PERSONAGENS_MAX',			370);
 
 define('MAX_POINTS_MANTRA',			20);
 define('MAX_POINTS_ARMAMENTO',		20);
