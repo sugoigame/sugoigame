ac3f213c4946faf22554af7a449fec96a3926c55
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-09 23:22:36 -0300

    Update pacote_comprar.php
    correção

--- public/Scripts/Vip/pacote_comprar.php
+++ public/Scripts/Vip/pacote_comprar.php
@@ -23,7 +23,7 @@ if (!$conect) {
     
         // Atualize a vantagem "Táticas" com a nova duração
         $tempo_base = $userDetails->vip["tatic"] > atual_segundo() ? $userDetails->vip["tatic_duracao"] : atual_segundo();
-        $tempo = $tempo_base + 2592000;
+        $tempo = $tempo_base + 30 * 24 * 60 * 60;;
         $connection->run("UPDATE tb_vip SET tatic = 1, tatic_duracao = ? WHERE id = ?",
             "ii", array($tempo, $userDetails->tripulacao["id"]));
     
