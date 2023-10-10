50ae910ec28150f50e97ad6fb60c388f7d29886b
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-09 23:30:33 -0300

    Update pacote_comprar.php
    corrigido

--- public/Scripts/Vip/pacote_comprar.php
+++ public/Scripts/Vip/pacote_comprar.php
@@ -22,7 +22,7 @@ if (!$conect) {
         $tempo_pacote = $tempo_base + 30 * 24 * 60 * 60;
     
         // Atualize a vantagem "Táticas" com a nova duração
-        $tempo_base_tatic = $userDetails->vip["tatic"] > atual_segundo() ? $userDetails->vip["tatic_duracao"] : atual_segundo();
+        $tempo_base_tatic = $userDetails->vip["tatic_duracao"] > atual_segundo() ? $userDetails->vip["tatic_duracao"] : atual_segundo();
         $tempo_tatic_nova = $tempo_base_tatic + 30 * 24 * 60 * 60;
         $connection->run("UPDATE tb_vip SET tatic = 1, tatic_duracao = ? WHERE id = ?",
             "ii", array($tempo_tatic_nova, $userDetails->tripulacao["id"]));
@@ -34,7 +34,7 @@ if (!$conect) {
             "ii", array($tempo_coup_nova, $userDetails->tripulacao["id"]));
     
         // Atualize a vantagem "Luneta" com a nova duração
-        $tempo_base_luneta = $userDetails->vip["luneta"] > atual_segundo() ? $userDetails->vip["luneta_duracao"] : atual_segundo();
+        $tempo_base_luneta = $userDetails->vip["luneta_duracao"] > atual_segundo() ? $userDetails->vip["luneta_duracao"] : atual_segundo();
         $tempo_luneta_nova = $tempo_base_luneta + 30 * 24 * 60 * 60;
         $connection->run("UPDATE tb_vip SET luneta = '1', luneta_duracao = ? WHERE id = ?",
             "ii", array($tempo_luneta_nova, $userDetails->tripulacao["id"]));
