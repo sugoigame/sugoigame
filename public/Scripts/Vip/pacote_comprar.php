6b3aa340469250946a1250c524aec5fad933fb27
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-09 22:49:48 -0300

    Update pacote_comprar.php
    atualização

--- public/Scripts/Vip/pacote_comprar.php
+++ public/Scripts/Vip/pacote_comprar.php
@@ -14,9 +14,40 @@ function comprarPacoteVip() {
     $protector->need_gold(PRECO_GOLD_PACOTE);
 
     // Defina o tempo de duração
-    $tempo = time() + 30 * 24 * 60 * 60;
+    $tempo_base = $userDetails->vip["pacote"] ? $userDetails->vip["pacote_duracao"] : atual_segundo();
+    $tempo_pacote = $tempo_base + 30 * 24 * 60 * 60;
 
-    // Comprar coup de burst
+    // Verifique se o usuário já possui a vantagem "Táticas"
+    if ($userDetails->vip["tatic"]) {
+        // Se possui, adicione 30 dias de duração
+        $tempo_pacote += 30 * 24 * 60 * 60;
+    } else {
+        // Se não possui, adicione a vantagem "Táticas" com 30 dias de duração
+        $connection->run("UPDATE tb_vip SET tatic = 1, tatic_duracao = ? WHERE id = ?",
+            "ii", array($tempo_pacote, $userDetails->tripulacao["id"]));
+    }
+
+    // Verifique se o usuário já possui a vantagem "Coup de Burst"
+    if ($userDetails->vip["coup_de_burst"]) {
+        // Se possui, adicione 30 dias de duração
+        $tempo_pacote += 30 * 24 * 60 * 60;
+    } else {
+        // Se não possui, adicione a vantagem "Coup de Burst" com 30 dias de duração
+        $connection->run("UPDATE tb_vip SET coup_de_burst = 1, coup_de_burst_duracao = ? WHERE id = ?",
+            "ii", array($tempo_pacote, $userDetails->tripulacao["id"]));
+    }
+
+    // Verifique se o usuário já possui a vantagem "Luneta"
+    if ($userDetails->vip["luneta"]) {
+        // Se possui, adicione 30 dias de duração
+        $tempo_pacote += 30 * 24 * 60 * 60;
+    } else {
+        // Se não possui, adicione a vantagem "Luneta" com 30 dias de duração
+        $connection->run("UPDATE tb_vip SET luneta = 1, luneta_duracao = ? WHERE id = ?",
+            "ii", array($tempo_pacote, $userDetails->tripulacao["id"]));
+    }
+    
+    /*Comprar coup de burst
     $connection->run("UPDATE tb_vip SET coup_de_burst = 5, coup_de_burst_duracao = ? WHERE id = ?",
     "ii", array($tempo, $userDetails->tripulacao["id"]));
 
@@ -28,7 +59,7 @@ function comprarPacoteVip() {
 
     // Comprar taticas 
     $query = "UPDATE tb_vip SET tatic='1', tatic_duracao='$tempo' WHERE id='" . $usuario["id"] . "'";
-    mysql_query($query) or die("Não foi possível cadastrar o item");
+    mysql_query($query) or die("Não foi possível cadastrar o item");*/
 
     // Reduzir o valor do pacote vip
     $userDetails->reduz_gold(PRECO_GOLD_PACOTE, "pacote_vip");
