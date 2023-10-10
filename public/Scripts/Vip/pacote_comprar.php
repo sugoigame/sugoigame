95b702548bf748cddd71c6bf45ef97e10ae477d7
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-09 23:09:25 -0300

    Update pacote_comprar.php
    teste

--- public/Scripts/Vip/pacote_comprar.php
+++ public/Scripts/Vip/pacote_comprar.php
@@ -92,7 +92,7 @@ if (!$conect) {
     
         // Atualize a vantagem "Luneta" com a nova duração
         $tempoBase  = $userDetails->vip["luneta"] ? $userDetails->vip["luneta_duracao"] : atual_segundo();
-        $tempo      = $tempoBase + (30 * 86400); // 86400 = 1 dia
+        $tempo      = $tempoBase + + 30 * 24 * 60 * 60; //(30 * 86400); // 86400 = 1 dia
         $connection->run("UPDATE tb_vip SET luneta = '1', luneta_duracao = ? WHERE id = ?", 'ii', [
             $tempo,
             $usuario['id']
