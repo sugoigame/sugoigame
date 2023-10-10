fdc83a456e8502b9bbda70e4c920621b3d4057aa
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-09 22:56:46 -0300

    Update pacote_comprar.php
    teste

--- public/Scripts/Vip/pacote_comprar.php
+++ public/Scripts/Vip/pacote_comprar.php
@@ -7,7 +7,7 @@ if (!$conect) {
     exit();
 }
 
-function comprarPacoteVip() {
+/*function comprarPacoteVip() {
     global $userDetails, $connection, $protector;
 
     // Verifique apenas o preço do gol de cada vantagem
@@ -61,12 +61,44 @@ function comprarPacoteVip() {
     $query = "UPDATE tb_vip SET tatic='1', tatic_duracao='$tempo' WHERE id='" . $usuario["id"] . "'";
     mysql_query($query) or die("Não foi possível cadastrar o item");*/
 
-    // Reduzir o valor do pacote vip
-    $userDetails->reduz_gold(PRECO_GOLD_PACOTE, "pacote_vip");
+    // Reduzir o valor do pacote vip*/
 
-    echo ("- Parabébns!<br>Você acabou de comprar o Pacote De Vantagens!");
-
-}
+    function comprarPacoteVip() {
+        global $userDetails, $connection, $protector;
+    
+        // Verifique apenas o preço do gol de cada vantagem
+        $protector->need_gold(PRECO_GOLD_PACOTE);
+    
+        // Obtenha a duração atual de cada vantagem
+        $tempo_taticas = $userDetails->vip["tatic_duracao"];
+        $tempo_coup = $userDetails->vip["coup_de_burst_duracao"];
+        $tempo_luneta = $userDetails->vip["luneta_duracao"];
+    
+        // Defina o tempo base para o "PACOTE VIP"
+        $tempo_base = max($tempo_taticas, $tempo_coup, $tempo_luneta);
+        $tempo_pacote = $tempo_base + 30 * 24 * 60 * 60;
+    
+        // Atualize a vantagem "Táticas" com a nova duração
+        $tempo_taticas = $tempo_base + 30 * 24 * 60 * 60;
+        $connection->run("UPDATE tb_vip SET tatic_duracao = ? WHERE id = ?",
+            "ii", array($tempo_taticas, $userDetails->tripulacao["id"]));
+    
+        // Atualize a vantagem "Coup de Burst" com a nova duração
+        $tempo_coup = $tempo_base + 30 * 24 * 60 * 60;
+        $connection->run("UPDATE tb_vip SET coup_de_burst_duracao = ? WHERE id = ?",
+            "ii", array($tempo_coup, $userDetails->tripulacao["id"]));
+    
+        // Atualize a vantagem "Luneta" com a nova duração
+        $tempo_luneta = $tempo_base + 30 * 24 * 60 * 60;
+        $connection->run("UPDATE tb_vip SET luneta_duracao = ? WHERE id = ?",
+            "ii", array($tempo_luneta, $userDetails->tripulacao["id"]));
+    
+        // Reduzir o valor do pacote vip
+        $userDetails->reduz_gold(PRECO_GOLD_PACOTE, "pacote_vip");
+    
+        echo ("- Parabébns!<br>Você acabou de comprar o Pacote De Vantagens!");
+    }
+    
 
 comprarPacoteVip();
 ?> 
