6c862fa19d3cc0cb296863de22d1c0b47d41e4f7
Ivan Miranda <ivan.i.n@hotmail.com>
2024-02-12 20:53:03 -0300

    fix icones de compartilhamento quebrado

--- public/Classes/Response.php
+++ public/Classes/Response.php
@@ -40,37 +40,6 @@ class Response
 
     public function share_msg($msg, $urlToShare = "https://sugoigame.com.br")
     {
-
-        return $msg . "<style type='text/css'>
-								ul.share-buttons{
-									list-style: none;
-									padding: 0;
-									margin-top: 2em;
-								}
-
-								ul.share-buttons li{
-									display: inline;
-								}
-
-								ul.share-buttons .sr-only{
-									position: absolute;
-									clip: rect(1px 1px 1px 1px);
-									clip: rect(1px, 1px, 1px, 1px);
-									padding: 0;
-									border: 0;
-									height: 1px;
-									width: 1px;
-									overflow: hidden;
-								}
-
-								ul.share-buttons img{
-									width: 32px;
-								}
-							</style>
-
-							<ul class=\"share-buttons\">
-									<li><a href=\"https://www.facebook.com/sharer/sharer.php?u=" . urlencode($urlToShare) . "&t=" . urlencode($msg) . "\" title=\"Compartilhe no Facebook\" target=\"_blank\"><img alt=\"Share on Facebook\" src=\"Imagens/Social/Facebook.svg\" /></a></li>
-									<li><a href=\"https://twitter.com/intent/tweet?original_referer=" . urlencode($urlToShare) . "&text=Sugoi Game - " . urlencode($urlToShare) . "\" target=\"_blank\" title=\"Tweet\"><img alt=\"Tweet\" src=\"Imagens/Social/Twitter.svg\" /></a></li>
-							</ul>";
+        return $msg;
     }
 }
