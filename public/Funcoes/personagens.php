910ed0c6e617a955ec9b6770f1edf70a0f22c188
Ivan Miranda <ivan.i.n@hotmail.com>
2024-02-14 21:34:12 -0300

    remove barra de energia do jogo

--- public/Funcoes/personagens.php
+++ public/Funcoes/personagens.php
@@ -251,7 +251,6 @@ function preco_selo_exp($pers)
 { ?>
     <div class="clearfix">
         <?php render_personagem_hp_bar($pers, $text); ?>
-        <?php render_personagem_mp_bar($pers, $text); ?>
         <?php render_personagem_xp_bar($pers, $text); ?>
     </div>
 <?php } ?>
@@ -264,20 +263,6 @@ function preco_selo_exp($pers)
             <?php if ($pers["id"] == $userDetails->tripulacao["id"] && $text) : ?>
                 <span>HP:
                     <?= $pers["hp"] . "/" . $pers["hp_max"] ?>
-                </span>
-            <?php endif; ?>
-        </div>
-    </div>
-<?php } ?>
-<?php function render_personagem_mp_bar($pers, $text = true)
-{ ?>
-    <?php global $userDetails; ?>
-    <div class="progress mp-bar">
-        <div class="progress-bar progress-bar-warning" role="progressbar"
-            style="width: <?= $pers["mp"] / $pers["mp_max"] * 100 ?>%;">
-            <?php if ($pers["id"] == $userDetails->tripulacao["id"] && $text) : ?>
-                <span>Energia:
-                    <?= $pers["mp"] . "/" . $pers["mp_max"] ?>
                 </span>
             <?php endif; ?>
         </div>
--- public/Scripts/Batalha/batalha_skils.php
+++ public/Scripts/Batalha/batalha_skils.php
@@ -5,22 +5,22 @@ require "../../Includes/conectdb.php";
 $protector->need_tripulacao();
 $protector->must_be_in_any_kind_of_combat();
 
-if (!validate_number($_GET["cod"])) {
-    echo("#Você informou algum caracter inválido.");
+if (! validate_number($_GET["cod"])) {
+    echo ("#Você informou algum caracter inválido.");
     exit();
 }
 $cod = $_GET["cod"];
 
 $result = $connection->run(
-    "SELECT * 
+    "SELECT *
     FROM tb_combate_personagens cbtpers
     INNER JOIN tb_personagens pers ON cbtpers.cod = pers.cod
     WHERE cbtpers.id = ? AND cbtpers.cod = ?",
     "ii", array($userDetails->tripulacao["id"], $cod)
 );
 
-if (!$result->count()) {
-    echo("#Personagem inválido.");
+if (! $result->count()) {
+    echo ("#Personagem inválido.");
     exit();
 }
 $pers = $result->fetch_array();
@@ -36,31 +36,29 @@ $skills = get_many_results_joined_mapped_by_type("tb_personagens_skil", "cod_ski
 
 
 ?>
-<div>
-    <?php render_personagem_mp_bar($pers); ?>
-</div>
 <div class="clearfix">
     <?php $last_tipo = NULL; ?>
-    <?php foreach ($skills as $skill): ?>
+    <?php foreach ($skills as $skill) : ?>
         <?php
         $result = $connection->run(
-            "SELECT * 
-             FROM tb_combate_skil_espera 
+            "SELECT *
+             FROM tb_combate_skil_espera
 			 WHERE cod = ? AND cod_skil = ? AND tipo = ?",
             "iii", array($cod, $skill["cod_skil"], $skill["tipo"])
         );
         $espera = $result->count() ? $result->fetch_array() : array("espera" => 0);
         ?>
-        <?php if ($skill["tipo"] != $last_tipo): ?>
+        <?php if ($skill["tipo"] != $last_tipo) : ?>
             <?php $last_tipo = $skill["tipo"]; ?>
-            <h5><?= nome_tipo_skill($skill) ?> de <?= nome_origem_skill($skill) ?></h5>
+            <h5>
+                <?= nome_tipo_skill($skill) ?> de
+                <?= nome_origem_skill($skill) ?>
+            </h5>
         <?php endif; ?>
         <div class="icon-skill-cbt">
             <div>
-                <a href="#" class="noHref" data-toggle="popover" data-content="-" data-placement="bottom"
-                   data-html="true"
-                   data-trigger="focus"
-                   data-template='<div class="popover-skill">
+                <a href="#" class="noHref" data-toggle="popover" data-content="-" data-placement="bottom" data-html="true"
+                    data-trigger="focus" data-template='<div class="popover-skill">
                             <h4>
                                 <?= str_replace("'", "&rsquo;", $skill["nome"]) ?>
                                 <img src="Imagens/Icones/tipo_<?= nome_tipo_skill($skill) ?>.png" data-toggle="tooltip" data-placement="bottom"
@@ -69,7 +67,7 @@ $skills = get_many_results_joined_mapped_by_type("tb_personagens_skil", "cod_ski
                             <h5><?= nome_tipo_skill($skill) ?> de <?= nome_origem_skill($skill) ?></h5>
                             <div class="text-left clearfix">
                                 <?php render_skill_efeitos($skill) ?>
-                                <?php if ($skill["special_effect"]): ?>
+                                <?php if ($skill["special_effect"]) : ?>
                                     <p>
                                         <?= nome_special_effect_apply_type($skill["special_apply_type"]) ?>
                                         <?= nome_special_effect($skill["special_effect"]) ?>
@@ -86,13 +84,15 @@ $skills = get_many_results_joined_mapped_by_type("tb_personagens_skil", "cod_ski
                 </a>
             </div>
             <div>
-                <?php if ($espera["espera"]): ?>
-                    <p><?= $espera["espera"] ?> turno(s)</p>
-                <?php elseif ($pers["mp"] < $skill["consumo"]): ?>
+                <?php if ($espera["espera"]) : ?>
+                    <p>
+                        <?= $espera["espera"] ?> turno(s)
+                    </p>
+                <?php elseif ($pers["mp"] < $skill["consumo"]) : ?>
                     <p>Energia insuficiente</p>
-                <?php elseif (!$espera["espera"] && $pers["mp"] >= $skill["consumo"]): ?>
+                <?php elseif (! $espera["espera"] && $pers["mp"] >= $skill["consumo"]) : ?>
                     <button class="btn btn-success" data-dismiss="modal"
-                            onclick="usaSkil('<?= $skill["cod_skil"]; ?>','<?= $pers["cod"]; ?>','<?= $skill["alcance"]; ?>', '<?= $skill["tipo"]; ?>','<?= $skill["area"]; ?>')">
+                        onclick="usaSkil('<?= $skill["cod_skil"]; ?>','<?= $pers["cod"]; ?>','<?= $skill["alcance"]; ?>', '<?= $skill["tipo"]; ?>','<?= $skill["area"]; ?>')">
                         Usar
                     </button>
                 <?php endif; ?>
@@ -108,20 +108,18 @@ $skills = get_many_results_joined_mapped_by_type("tb_personagens_skil", "cod_ski
         <?php foreach ($items as $item) : ?>
             <?php
             $result = $connection->run(
-                "SELECT * 
-                 FROM tb_combate_skil_espera 
+                "SELECT *
+                 FROM tb_combate_skil_espera
                  WHERE id = ? AND tipo = ?",
                 "ii", array($userDetails->tripulacao["id"], 10)
             );
             $espera = $result->count() ? $result->fetch_array() : array("espera" => 0);
             $consumo = ($item["hp_recuperado"] + $item["mp_recuperado"])
-            ?>
+                ?>
             <div class="icon-skill-cbt">
                 <div>
-                    <a href="#" class="noHref" data-toggle="popover" data-content="-" data-placement="bottom"
-                       data-html="true"
-                       data-trigger="focus"
-                       data-template='<div class="popover-skill">
+                    <a href="#" class="noHref" data-toggle="popover" data-content="-" data-placement="bottom" data-html="true"
+                        data-trigger="focus" data-template='<div class="popover-skill">
                             <?= info_item($item, $item, FALSE, FALSE, FALSE) ?>
                             <p>Espera: 2 turnos</p>
                             <p>Consumo: <?= $consumo ?></p>
@@ -130,15 +128,17 @@ $skills = get_many_results_joined_mapped_by_type("tb_personagens_skil", "cod_ski
                     </a>
                 </div>
                 <div>
-                    <?php if ($espera["espera"]): ?>
-                        <p><?= $espera["espera"] ?> turno(s)</p>
-                    <?php elseif ($pers["profissao_lvl"] < $item["requisito_lvl"]): ?>
+                    <?php if ($espera["espera"]) : ?>
+                        <p>
+                            <?= $espera["espera"] ?> turno(s)
+                        </p>
+                    <?php elseif ($pers["profissao_lvl"] < $item["requisito_lvl"]) : ?>
                         <p>Nível Baixo</p>
-                    <?php elseif ($pers["mp"] < $consumo): ?>
+                    <?php elseif ($pers["mp"] < $consumo) : ?>
                         <p>Energia insuficiente</p>
-                    <?php elseif (!$espera["espera"] && $pers["mp"] >= $consumo && $pers["profissao_lvl"] >= $item["requisito_lvl"]): ?>
+                    <?php elseif (! $espera["espera"] && $pers["mp"] >= $consumo && $pers["profissao_lvl"] >= $item["requisito_lvl"]) : ?>
                         <button class="btn btn-success" data-dismiss="modal"
-                                onclick="usaSkil('<?= $item["cod_remedio"]; ?>','<?= $pers["cod"]; ?>','1', '10','1')">
+                            onclick="usaSkil('<?= $item["cod_remedio"]; ?>','<?= $pers["cod"]; ?>','1', '10','1')">
                             Usar
                         </button>
                     <?php endif; ?>
