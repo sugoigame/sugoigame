fce835360dc6c8891acf695f69a845d3747ecaee
Ivan Miranda <ivan.i.n@hotmail.com>
2024-02-18 12:37:37 -0300

    add habilidades musico

--- public/Scripts/Personagem/status/profissao.php
+++ public/Scripts/Personagem/status/profissao.php
@@ -5,12 +5,13 @@ $pers_cod = $protector->get_number_or_exit("cod");
 
 $pers = $userDetails->get_pers_by_cod($pers_cod);
 
-if (!$pers) {
+if (! $pers) {
     $protector->exit_error("Personagem inválido");
 }
 ?>
 
-<?php function ordena_receitas($receitas, $aleatorios) {
+<?php function ordena_receitas($receitas, $aleatorios)
+{
     foreach ($receitas as $index => $receita) {
         if ($receita["aleatorio"]) {
             $receitas[$index]["resultados"] = array();
@@ -24,20 +25,25 @@ if (!$pers) {
     return $receitas;
 } ?>
 
-<?php function render_item($cod, $tipo, $quant, $materiais, $equipamentos) { ?>
-    <?php if ($tipo == TIPO_ITEM_REAGENT): ?>
-        <button class="inventario-item equipamentos_casse_1"
-                data-content="-" data-toggle="popover" data-html="true" data-placement="bottom" data-trigger="focus"
-                data-template='<div class="inventario-item-info"><?= $materiais[$cod]["nome"] ?></div>'>
+<?php function render_item($cod, $tipo, $quant, $materiais, $equipamentos)
+{ ?>
+    <?php if ($tipo == TIPO_ITEM_REAGENT) : ?>
+        <button class="inventario-item equipamentos_casse_1" data-content="-" data-toggle="popover" data-html="true"
+            data-placement="bottom" data-trigger="focus"
+            data-template='<div class="inventario-item-info"><?= $materiais[$cod]["nome"] ?></div>'>
             <?= get_img_item($materiais[$cod]) ?>
-            <span class="badge badge-default"><?= $quant ?></span>
+            <span class="badge badge-default">
+                <?= $quant ?>
+            </span>
         </button>
-    <?php elseif ($tipo == TIPO_ITEM_EQUIPAMENTO): ?>
-        <button class="inventario-item equipamentos_casse_<?= $equipamentos[$cod]["categoria"] ?>"
-                data-content="-" data-toggle="popover" data-html="true" data-placement="bottom" data-trigger="focus"
-                data-template='<div class="inventario-item-info"><?= $equipamentos[$cod]["nome"] ?> - <?= nome_slot($equipamentos[$cod]["slot"]) ?> - Nível <?= $equipamentos[$cod]["lvl"] ?> (<?= nome_atributo($equipamentos[$cod]["b_1"]) . " e " . nome_atributo($equipamentos[$cod]["b_2"]) ?>)</div>'>
-            <img src="Imagens/Itens/<?= $equipamentos[$cod]["img"] ?>.png"/><br/>
-            <span class="badge badge-default"><?= $quant ?></span>
+    <?php elseif ($tipo == TIPO_ITEM_EQUIPAMENTO) : ?>
+        <button class="inventario-item equipamentos_casse_<?= $equipamentos[$cod]["categoria"] ?>" data-content="-"
+            data-toggle="popover" data-html="true" data-placement="bottom" data-trigger="focus"
+            data-template='<div class="inventario-item-info"><?= $equipamentos[$cod]["nome"] ?> - <?= nome_slot($equipamentos[$cod]["slot"]) ?> - Nível <?= $equipamentos[$cod]["lvl"] ?> (<?= nome_atributo($equipamentos[$cod]["b_1"]) . " e " . nome_atributo($equipamentos[$cod]["b_2"]) ?>)</div>'>
+            <img src="Imagens/Itens/<?= $equipamentos[$cod]["img"] ?>.png" /><br />
+            <span class="badge badge-default">
+                <?= $quant ?>
+            </span>
         </button>
     <?php endif; ?>
 <?php } ?>
@@ -56,7 +62,9 @@ if (!$pers) {
             && $userDetails->tripulacao["berries"] >= $skill["requisito_berries"]
             && $pers["profissao"] == $skill["requisito_prof"];
     }; ?>
-    <?php render_habilidades_tab($skills, $pers, "Profissao/aprender_skil_prof.php", $pode_aprender_func) ?>
+    <div class="row">
+        <?php render_habilidades_tab($skills, $pers, "Profissao/aprender_skil_prof.php", $pode_aprender_func) ?>
+    </div>
 <?php endif; ?>
 
 <?php if ($pers["profissao"] == PROFISSAO_MEDICO) : ?>
@@ -72,15 +80,15 @@ if (!$pers) {
                     <?= $item["requisito_lvl"]; ?>
                 </p>
                 <div>
-                    <img src="Imagens/Icones/Berries.png"/> <?= mascara_berries($preco) ?>
+                    <img src="Imagens/Icones/Berries.png" />
+                    <?= mascara_berries($preco) ?>
                 </div>
                 <?php if ($userDetails->tripulacao["berries"] >= $preco && $pers["profissao_lvl"] >= $item["requisito_lvl"]) : ?>
-                    <form action="Scripts/Profissao/medico_criar_remedio.php?pers=<?= $pers["cod"]; ?>&item=<?= $item["cod_remedio"] ?>"
-                          method="post">
-                        <input placeholder="Insira a quantidade desejada" class="form-control" size="4"
-                               min="1" type="number"
-                               max="<?= floor($userDetails->tripulacao["berries"] / $preco) ?>"
-                               name="quant" value="1" type="number">
+                    <form
+                        action="Scripts/Profissao/medico_criar_remedio.php?pers=<?= $pers["cod"]; ?>&item=<?= $item["cod_remedio"] ?>"
+                        method="post">
+                        <input placeholder="Insira a quantidade desejada" class="form-control" size="4" min="1" type="number"
+                            max="<?= floor($userDetails->tripulacao["berries"] / $preco) ?>" name="quant" value="1" type="number">
                         <button type="submit" class="btn btn-success">
                             Fazer
                         </button>
@@ -103,15 +111,15 @@ if (!$pers) {
                     <?= $item["requisito_lvl"]; ?>
                 </p>
                 <div>
-                    <img src="Imagens/Icones/Berries.png"/> <?= mascara_berries($preco) ?>
+                    <img src="Imagens/Icones/Berries.png" />
+                    <?= mascara_berries($preco) ?>
                 </div>
                 <?php if ($userDetails->tripulacao["berries"] >= $preco && $pers["profissao_lvl"] >= $item["requisito_lvl"]) : ?>
-                    <form action="Scripts/Profissao/cozinheiro_criar_comida.php?pers=<?= $pers["cod"]; ?>&item=<?= $item["cod_comida"] ?>"
-                          method="post">
-                        <input placeholder="Insira a quantidade desejada" class="form-control" size="4"
-                               min="1" type="number"
-                               max="<?= floor($userDetails->tripulacao["berries"] / $preco) ?>"
-                               name="quant" value="1" type="number">
+                    <form
+                        action="Scripts/Profissao/cozinheiro_criar_comida.php?pers=<?= $pers["cod"]; ?>&item=<?= $item["cod_comida"] ?>"
+                        method="post">
+                        <input placeholder="Insira a quantidade desejada" class="form-control" size="4" min="1" type="number"
+                            max="<?= floor($userDetails->tripulacao["berries"] / $preco) ?>" name="quant" value="1" type="number">
                         <button type="submit" class="btn btn-success">
                             Fazer
                         </button>
@@ -177,18 +185,22 @@ if (!$pers) {
 
     <h3>Receitas disponíveis:</h3>
     <ul class="list-group">
-        <?php foreach ($receitas as $receita): ?>
-            <?php if ($receita["lvl"] <= $pers["profissao_lvl"]): ?>
+        <?php foreach ($receitas as $receita) : ?>
+            <?php if ($receita["lvl"] <= $pers["profissao_lvl"]) : ?>
                 <li class="list-group-item">
-                    <h4>Nível <?= $receita["lvl"] ?></h4>
+                    <h4>Nível
+                        <?= $receita["lvl"] ?>
+                    </h4>
                     <div class="row">
                         <div class="col-md-6">
                             <h4>Itens necessários:</h4>
                             <div class="row">
-                                <?php for ($i = 1; $i <= 8; $i++): ?>
-                                    <?php if ($receita[$i]): ?>
+                                <?php for ($i = 1; $i <= 8; $i++) : ?>
+                                    <?php if ($receita[$i]) : ?>
                                         <div style="display: inline-block;">
-                                            <p>Espaço <?= $i ?>:</p>
+                                            <p>Espaço
+                                                <?= $i ?>:
+                                            </p>
                                             <?php render_item($receita[$i], $receita[$i . "_t"], $receita[$i . "_q"], $materiais, $equipamentos); ?>
                                         </div>
                                     <?php endif; ?>
@@ -198,11 +210,11 @@ if (!$pers) {
                         <div class="col-md-6">
                             <h4>Resultados possíveis:</h4>
                             <div class="row">
-                                <?php if ($receita["aleatorio"]): ?>
-                                    <?php foreach ($receita["resultados"] as $resultado): ?>
+                                <?php if ($receita["aleatorio"]) : ?>
+                                    <?php foreach ($receita["resultados"] as $resultado) : ?>
                                         <?php render_item($resultado["cod"], $resultado["tipo"], $resultado["quant"], $materiais, $equipamentos); ?>
                                     <?php endforeach; ?>
-                                <?php else: ?>
+                                <?php else : ?>
                                     <?php render_item($receita["cod"], $receita["tipo"], $receita["quant"], $materiais, $equipamentos); ?>
                                 <?php endif; ?>
                             </div>
@@ -213,3 +225,4 @@ if (!$pers) {
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
+
