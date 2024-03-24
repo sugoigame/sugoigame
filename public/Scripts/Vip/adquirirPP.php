7a55e76ca3bda04ba437ad8cd47d6345f4aadb03
Ivan Miranda <ivan.i.n@hotmail.com>
2024-03-24 13:38:39 -0300

    change paypal email

--- public/Scripts/Vip/adquirirPP.php
+++ public/Scripts/Vip/adquirirPP.php
@@ -3,44 +3,44 @@ require "../../Includes/conectdb.php";
 
 $protector->need_conta();
 
-$idPlano	= base64_decode($_GET['plano']);
-$method		= $protector->get_alphanumeric_or_exit('method');
-$coins		= [
-	'pagseguro'		=> 'BRL',
-	'paypal_eur'	=> 'EUR',
-	'paypal_usd'	=> 'USD',
-	// 'paypal_brl'	=> 'BRL'
+$idPlano = base64_decode($_GET['plano']);
+$method = $protector->get_alphanumeric_or_exit('method');
+$coins = [
+    'pagseguro' => 'BRL',
+    'paypal_eur' => 'EUR',
+    'paypal_usd' => 'USD',
+    // 'paypal_brl'	=> 'BRL'
 ];
 
 $result = $connection->run("SELECT * FROM tb_vip_planos WHERE id = ?", 'i', $idPlano);
 if ($result->count() < 1) {
-	header("Location: ../../?ses=vipComprar&msg=" . urlencode('Escolha um plano válido!') . "&");
-	exit;
-} elseif (!array_key_exists($method, $coins)) {
-	header("Location: ../../?ses=vipComprar&msg=" . urlencode('Escolha um metódo de pagamento válido!') . "&");
-	exit;
+    header("Location: ../../?ses=vipComprar&msg=" . urlencode('Escolha um plano válido!') . "&");
+    exit;
+} elseif (! array_key_exists($method, $coins)) {
+    header("Location: ../../?ses=vipComprar&msg=" . urlencode('Escolha um metódo de pagamento válido!') . "&");
+    exit;
 } else {
-	$plano =$result->fetch();
+    $plano = $result->fetch();
 
-	require_once('../../Classes/PayPal.php');
-	$p = new PayPal();
+    require_once ('../../Classes/PayPal.php');
+    $p = new PayPal();
 
-	// Inserir log de compra e definir referencia para pagamento
-	$connection->run("INSERT INTO tb_vip_compras (conta_id,plano_id,gateway) VALUE (?,?,?)", "iis", [
-		$userDetails->conta['conta_id'],
-		$plano['id'],
-		'PayPal ' . $coins[$method]
-	]);
-	$paymentId = $connection->last_id();
+    // Inserir log de compra e definir referencia para pagamento
+    $connection->run("INSERT INTO tb_vip_compras (conta_id,plano_id,gateway) VALUE (?,?,?)", "iis", [
+        $userDetails->conta['conta_id'],
+        $plano['id'],
+        'PayPal ' . $coins[$method]
+    ]);
+    $paymentId = $connection->last_id();
 
-	$p->addField('business',		'medeiros.dev@gmail.com');
-	$p->addField('return',			'https://' . $_SERVER['HTTP_HOST'] . '/?ses=vipComprar&success');
-	$p->addField('cancel_return',	'https://' . $_SERVER['HTTP_HOST'] . '/?ses=vipComprar&cancel');
-	$p->addField('notify_url',		'https://' . $_SERVER['HTTP_HOST'] . '/Scripts/PayPal/retorno.php');
-	$p->addField('item_name',		'Sugoi Game - ' . $plano['nome']);
-	$p->addField('currency_code',	$coins[$method]);
-	$p->addField('amount',			$plano['valor_' . strtolower($coins[$method])]);
-	$p->addField('custom',			$paymentId);
+    $p->addField('business', 'ivan.i.n@hotmail.com');
+    $p->addField('return', 'https://' . $_SERVER['HTTP_HOST'] . '/?ses=vipComprar&success');
+    $p->addField('cancel_return', 'https://' . $_SERVER['HTTP_HOST'] . '/?ses=vipComprar&cancel');
+    $p->addField('notify_url', 'https://' . $_SERVER['HTTP_HOST'] . '/Scripts/PayPal/retorno.php');
+    $p->addField('item_name', 'Sugoi Game - ' . $plano['nome']);
+    $p->addField('currency_code', $coins[$method]);
+    $p->addField('amount', $plano['valor_' . strtolower($coins[$method])]);
+    $p->addField('custom', $paymentId);
 
-	$p->submitPayment();	# submit the fields to paypal
+    $p->submitPayment();	# submit the fields to paypal
 }
