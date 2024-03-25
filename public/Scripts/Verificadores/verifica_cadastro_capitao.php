2b5fc887b0154b0432038e4a4cf4730e7f60381c
Ivan Miranda <ivan.i.n@hotmail.com>
2024-03-25 16:50:48 -0300

    fix bug de nome do personagem

--- public/Scripts/Verificadores/verifica_cadastro_capitao.php
+++ public/Scripts/Verificadores/verifica_cadastro_capitao.php
@@ -3,21 +3,21 @@ $valida = "EquipeSugoiGame2012";
 include "../../Includes/conectdb.php";
 
 if (! isset($_GET["capitao"])) {
-	echo 0;
-	exit();
+    echo 0;
+    exit();
 }
-$capitao = $protector->get_alphanumeric_or_exit("capitao");
+$capitao = $_GET["capitao"];
 if (! preg_match("/^[\w ]+$/", $capitao)) {
-	echo 0;
-	exit();
+    echo 0;
+    exit();
 }
 $sql = "SELECT * FROM tb_personagens WHERE nome='$capitao'";
 $result = $connection->run($sql);
 $cont = $result->count();
-if ($cont == 0)
-	echo 1;
-else
-	echo 0;
+if ($cont == 0) {
+    echo 1;
+} else {
+    echo 0;
+}
 
-?>
 
