ceac24f4c03e5ed7a589625fea6b557e1d42a6cc
Ivan Miranda <ivan.i.n@hotmail.com>
2024-07-03 00:55:29 -0300

    add habilidades perna negra

--- public/Scripts/Academia/academia_aprender.php
+++ public/Scripts/Academia/academia_aprender.php
@@ -10,25 +10,6 @@ $class = $protector->get_enum_or_exit("class", array_keys($classes));
 $personagem = $protector->get_tripulante_or_exit("cod");
 
 $classe = $classes[$class];
-// $query = "INSERT IGNORE INTO tb_personagens_skil (cod_pers, cod_skil, nome, descricao, icone, animacao) VALUES ";
-// $queries = [];
-// $values = [];
-// $formats = "";
-
-// foreach ($habilidades as $habilidade) {
-//     $habilidade = habilidade_default_values($habilidade);
-//     $queries[] = "(?,?,?,?,?,?)";
-//     $values[] = $personagem["cod"];
-//     $values[] = $habilidade["cod"];
-//     $values[] = $habilidade["nome"];
-//     $values[] = $habilidade["descricao"];
-//     $values[] = $habilidade["icone"];
-//     $values[] = $habilidade["animacao"];
-
-//     $formats .= "iissis";
-// }
-
-// $connection->run($query . implode(",", $queries), $formats, $values);
 
 $query = "UPDATE tb_personagens SET classe='$class' WHERE cod='" . $personagem["cod"] . "'";
 $connection->run($query);
